58b0f7e81033f6d829410f90f1e3e288f2927433
Facundo Sa <facundo.sa@web.com>
2018-05-14 09:51:10 -0300

    Add project files.

---
+++ CityInfo.API/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CityInfo.API
+{
+    public class Program
+    {
+        /// <summary>
+        /// This method is the responsible for configuring and running the application
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+        /// <summary>
+        /// Web Host builder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHost BuildWebHost(string[] args) =>
+            /* In this case, we'll be running a Web Application, so we need to host it.
+             Because of that, an instance of WebHostBuilder is initialized.
+             As we are hosting a web application, we need a web server. ASP.NET Core is completely decoupled from the web server enviroment that host the application.
+             It actually ships with two different Http servers: WebListener, which is Windows-only web server and Kestrel a cross-platform web server. Kestrel is the default.
+
+            */
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                //This specifies the content root directory used by the web host
+                //The content root is the base path to any content used by the app, such as views and its web content.
+                //Content root <> Web root 
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                /* This line signifies that IIS express functions as a reverse proxy server for Kestrel. If you intentd to deploy
+                 your application on a Windows Server, you should run IIS as a reverse proxy server that manages and proxies
+                 requests to Kestrel
+                */
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ CityInfo.API/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CityInfo.API
+{
+    /// <summary>
+    /// This class is an entry point for the application.
+    /// </summary>
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // What is a service in .NET Core? It's a component that is intended for common consumption in an application: There is framework services, like, Identity,
+        // MVC, EF Core Services; and there're also application services, which are application-specific: A component to send mail.
+        // and there're others that are built-in like the ApplicationBuilder and a Logger
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+            /* We add this after the exception handler, so that middleware can potentially catch exceptions before
+             handling the request over to MVC and, more importantly, handle exceptions and return the correct response
+             when an exception happens in the MVC-related code we'll write.
+             At this moment, MVC middleware will handle MVC requests */
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+        }
+    }
+}
