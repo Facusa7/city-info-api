cbb1bf1f41c6fb32e18793e3eed4b21c85c8f2a8
Facundo Sa <facundo.sa@web.com>
2018-05-17 14:22:33 -0300

    Controller Added

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        /// <summary>
+        /// JsonResult class returns a JSONified version of whatever we pass into the constructor of JsonResult
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, name="Buenos Aires"},
+                new { id=2, name="Prague"},
+            });
+        }
+    }
+}
