f69a6cfb0fb6a31e67cf5e127679a1b4fa555281
Facundo Sa <facundo.sa@web.com>
2018-05-17 17:03:36 -0300

    Improving the controller

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        /// <summary>
+        ///This make sure that we can keep working on the same data as long as we don't restart.
+        /// </summary>
+        public static CitiesDataStore Current { get; } = new CitiesDataStore(); 
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Buenos Aires",
+                    Description = "The Paris of South America"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Resistencia",
+                    Description = "The City of Scultures"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Corrientes",
+                    Description = "The City with Paye"
+                }
+            };
+        }
+    }
+}
--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -6,19 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// By adding this annotation, we are saying that all requests with api/cities will be addressed in this controller. 
+    /// The annotation could be like this [Route("api/[controller]")], and for web sites is a good idea, but for APIs is not. 
+    /// Since the name of the controller could change if we did a refactor, it could affect to the consumers of this info, and that is not important to them.
+    /// </summary>
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
         /// <summary>
         /// JsonResult class returns a JSONified version of whatever we pass into the constructor of JsonResult
         /// </summary>
         /// <returns></returns>
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new { id=1, name="Buenos Aires"},
-                new { id=2, name="Prague"},
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public JsonResult Getcity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id));
         }
     }
 }
