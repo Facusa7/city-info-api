2ae8ccb983b0b40c07e0c7b5b88a493ecf63109b
Facundo Sa <facundo.sa@web.com>
2018-05-21 10:19:39 -0300

    Add method to get a single point of interest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -23,5 +23,22 @@ namespace CityInfo.API.Controllers
 
             return Ok(city.PointsOfInterest);
         }
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
     }
 }
