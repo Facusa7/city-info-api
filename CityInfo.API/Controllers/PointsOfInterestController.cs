f0eda77cdb78347650314d0e1090b8fa3ff3920a
Facundo Sa <facundo.sa@web.com>
2018-05-22 17:23:40 -0300

    Delete, update and partially update added to the API.

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -79,6 +80,134 @@ namespace CityInfo.API.Controllers
                Since GetPointOfInterest needs a cityId and the pointOfInterestId we pass that through, and also we send the object to show it. 
              */
             return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, id = finalPointOfInterest.Id}, finalPointOfInterest);
+        }
+        /// <summary>
+        /// HTTP Standard says that the the 'put' verb is for updating all values, like a full update. 
+        /// If the consumer doesn't provide a value for a field, that field should be put to its default value, which conveniently, it will
+        /// have in the inputted object.
+        /// </summary>
+        /// <param name="cityId"></param>
+        /// <param name="id"></param>
+        /// <param name="pointOfInterest"></param>
+        /// <returns></returns>
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            //TODO: Add Fluent Validation
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name");
+            }
+            //It does the validations that were set in the model
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            //NoContent() means that the request completed successfully, but there is nothing to return.
+            return NoContent();
+        }
+        /// <summary>
+        /// JsonPatchDocument allows us to partially update an object
+        /// A Patch request has this form 
+        ///    [    {
+        ///            "op":"replace", (operation)
+        ///            "path": "/name", (name of the value to change)
+        ///            "value": "updatedValue" (actual value) 
+        ///          } 
+        ///    ]
+        /// </summary>
+        /// <param name="cityId"></param>
+        /// <param name="id"></param>
+        /// <param name="patchDoc"></param>
+        /// <returns></returns>
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+                                            {
+                                                Name = pointOfInterestFromStore.Name,
+                                                Description = pointOfInterestFromStore.Description
+                                            };
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (pointOfInterestToPatch.Name == pointOfInterestToPatch.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name");
+            }
+            /* This is to validate that the document that is being passed is valid.
+             Since a delete operation could've been sent and this could cause invalid data in our models. */
+            TryValidateModel(pointOfInterestToPatch);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            //NoContent() means that the request completed successfully, but there is nothing to return.
+            return NoContent();
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
         }
     }
 }
