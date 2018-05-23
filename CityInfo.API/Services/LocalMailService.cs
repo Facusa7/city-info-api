15157898e991f2ecc4cc796d123f7cb2394a236b
Facundo Sa <facusa7@hotmail.com>
2018-05-22 22:19:10 -0300

    Dependency Injection with Settings and logging added

---
+++ CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    /// <summary>
+    /// For the purposes of this demo, the LocalMailService will be a dummy class with a mock send email method
+    /// </summary>
+    public class LocalMailService : IMailService
+    {
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with LocalMailService");
+            Debug.WriteLine($"Subject:  {subject}");
+            Debug.WriteLine($"Message:  {message}");
+        }
+    }
+}
