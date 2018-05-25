aeb5c0b1c25f58fafceb3cafda5cff439a7fcc79
Facundo Sa <facundo.sa@web.com>
2018-05-25 11:49:11 -0300

    Entity class were created

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        /// <summary>
+        /// When we declare an Id (City_Id will work as well) field in the EntityClass, the DB would assume that is the primary key, but
+        /// it won't hurt add the [Key] annotation here.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public ICollection<PointOfInterest> PointOfInterest { get; set; } = new List<PointOfInterest>();
+    }
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        /// <summary>
+        /// This is a convention-based to point a foreign key. A property is considered a navigation one if
+        /// the declaration here cannot be mapped as a scalar type by the current database provider. 
+        /// </summary>
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        /// <summary>
+        /// This is the way to explicit the foreign key. Even though is not necessary, it is a good way to make it clear. 
+        /// </summary>
+        public int CityId { get; set; }
+    }
+}
